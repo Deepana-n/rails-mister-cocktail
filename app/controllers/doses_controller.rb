f786c88bff4e8e1cc3659397e21e2ffebdb69eeb
Deepana Naidu <deepana.naidu@gmail.com>
2021-02-19 14:54:27 +0800

    completed backend logic

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,2 +1,28 @@
 class DosesController < ApplicationController
+  def index
+    @doses = Dose.all
+  end
+  def new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new
+  end
+
+  def create
+    dose_params = params.require(:dose).permit(:description, :ingredient_id)
+    @dose = Dose.new(dose_params)
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose.cocktail = @cocktail
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @dose = Dose.find(params[:id])
+    @cocktail = @dose.cocktail
+    @dose.destroy
+    redirect_to cocktail_path(@cocktail)
+  end
 end
