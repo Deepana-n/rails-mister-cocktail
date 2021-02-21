8f488fc200bd7c11c6ebe11f77016f36ee04511b
Deepana Naidu <deepana.naidu@gmail.com>
2021-02-22 00:07:08 +0800

    completed banner fix

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -24,6 +24,7 @@ class CocktailsController < ApplicationController
   def show
     @cocktail = Cocktail.find(params[:id])
     @doses = Dose.where(cocktail_id: params[:id])
+    @dose = Dose.new
   end
 
   def update
